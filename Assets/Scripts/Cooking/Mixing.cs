d781e943689ea33d188229bac4aac099c7e912ec
maklsmit <156136111+maklsmit@users.noreply.github.com>
2025-10-01 09:46:16 -0400

    Mixing try but buggy

--- Assets/Scripts/Cooking/Mixing.cs
+++ Assets/Scripts/Cooking/Mixing.cs
@@ -7,12 +7,15 @@ using UnityEngine.UI;
 public class Mixing : MonoBehaviour
 {
     [SerializeField] GameObject controlsText;
-    [SerializeField] Transform shaker;
-    [SerializeField] int totalMixes = 10;
+    // [SerializeField] Transform shaker;
+    [SerializeField] int totalMixes = 5;
+    [SerializeField] Vector2 center = new Vector2(0,0);
 
     Vector2 prevPos;
 
     int mixCount = 0;
+    float currentMix = 0;   //Once this reaches 2pi, mixCount++ and currentMix = 0
+    const float fullMixValue = 2 * Mathf.PI;
 
     void Start()
     {
@@ -24,20 +27,45 @@ public class Mixing : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateRotation(pos);
+            Debug.Log(currentMix + " is current mix");
+        }
 
+        if (currentMix >= fullMixValue)
+        {
+            mixCount++;
+            currentMix = 0;
         }
 
         if (mixCount >= totalMixes)
-        {
-            CookingManager.instance.Transition();
-            controlsText.SetActive(false);
-            gameObject.SetActive(false);
-        }
+            {
+                CookingManager.instance.Transition();
+                controlsText.SetActive(false);
+                gameObject.SetActive(false);
+            }
     }
-    
+
     /*
     while mousebutton, detect if mouse is going in a circle
 
 
     */
+    void CalculateRotation(Vector2 currentPos)
+    {
+        /*
+        Can do 2 ways:
+        1. Calculate Vector2.right to Vector2(prev -> center) and Vector2.right to Vector2(current -> center)
+           then take the difference in radians as the change
+        or
+        2. Calculate Vector2(prev -> center) to Vector2(current -> center)
+        */
+        float deltaX = currentPos.x - prevPos.x;
+        float deltaY = currentPos.y - prevPos.y;
+
+        float radians = Mathf.Atan2(deltaX, deltaY);
+        Debug.Log(radians + " is the radians");
+
+        currentMix += radians;
+    }
 }
